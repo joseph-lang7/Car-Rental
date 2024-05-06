a047e3910fd0af1e227a03e847691dabcfaa3b6f
joseph <langj6757@gmail.com>
2024-05-06 11:42:38 -0700

    Implement renting process section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import BookCar from "../components/BookCar";
 import Hero from "../components/Hero";
+import RentingProcess from "../components/RentingProcess";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <BookCar />
+      <RentingProcess />
     </>
   );
 };
