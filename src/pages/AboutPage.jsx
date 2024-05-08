e74f157dd3fe4f312e4c1c94305dce1229fbf7e8
joseph <langj6757@gmail.com>
2024-05-07 17:17:59 -0700

    Add rentingprocess component to about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,12 @@
 import AboutCompany from "../components/AboutCompany";
 import Hero2 from "../components/Hero2";
+import RentingProcess from "../components/RentingProcess";
 const AboutPage = () => {
   return (
     <div className="flex flex-col gap-[150px]">
       <Hero2 title="About" />
       <AboutCompany />
+      <RentingProcess />
     </div>
   );
 };
