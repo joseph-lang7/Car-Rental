2661ff2888784d123ece7c76aaa283ee82ad5b53
joseph <langj6757@gmail.com>
2024-05-07 17:34:49 -0700

    Create contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,19 @@
+import { FaPhoneAlt } from "react-icons/fa";
+
+const Contact = () => {
+  return (
+    <div className="max-w-screen h-auto p-10 bg-[#2D2D2D] flex justify-center ">
+      <div className="flex flex-col gap-2 md:flex-row max-w-[1500px]">
+        <h2 className="flex-1 text-white text-4xl text-center font-bold">
+          Book a car by getting in touch with us
+        </h2>
+        <h3 className="flex-1 text-[#FA4226] flex items-center gap-3 font-bold text-3xl justify-center">
+          <FaPhoneAlt size={30} />
+          (123) 456-7829
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,12 +1,14 @@
 import AboutCompany from "../components/AboutCompany";
 import Hero2 from "../components/Hero2";
 import RentingProcess from "../components/RentingProcess";
+import Contact from "../components/Contact";
 const AboutPage = () => {
   return (
     <div className="flex flex-col gap-[150px]">
       <Hero2 title="About" />
       <AboutCompany />
       <RentingProcess />
+      <Contact />
     </div>
   );
 };
